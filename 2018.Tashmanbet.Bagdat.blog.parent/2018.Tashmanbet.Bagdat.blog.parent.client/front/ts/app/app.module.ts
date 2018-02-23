65d0ef09e051dc5dc4c7a65d679f69bd5917f146
bagdaterlanuly <bagdat.tashmanbet@ce.sdu.edu.kz>
2018-02-23 14:00:26 +0600

    login page

--- 2018.Tashmanbet.Bagdat.blog.parent/2018.Tashmanbet.Bagdat.blog.parent.client/front/ts/app/app.module.ts
+++ 2018.Tashmanbet.Bagdat.blog.parent/2018.Tashmanbet.Bagdat.blog.parent.client/front/ts/app/app.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from "@angular/core"
 import {AppComponent} from "./app.component";
+import {BrowserModule} from "@angular/platform-browser";
 @NgModule({
     imports:[
+        BrowserModule
     ],
     declarations:[AppComponent],
     providers:[],
