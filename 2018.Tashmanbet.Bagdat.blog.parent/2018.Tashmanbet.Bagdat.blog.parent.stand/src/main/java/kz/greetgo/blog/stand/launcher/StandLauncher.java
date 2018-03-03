83be0f8708429fb7cc433af3e75eacfbcd44183d
bagdaterlanuly <bagdat.tashmanbet@ce.sdu.edu.kz>
2018-03-03 11:05:30 +0600

    login page

---
+++ 2018.Tashmanbet.Bagdat.blog.parent/2018.Tashmanbet.Bagdat.blog.parent.stand/src/main/java/kz/greetgo/blog/stand/launcher/StandLauncher.java
@@ -0,0 +1,21 @@
+package kz.greetgo.blog.stand.launcher;
+
+import kz.greetgo.depinject.Depinject;
+import kz.greetgo.depinject.gen.DepinjectUtil;
+import kz.greetgo.blog.stand.utils.Modules;
+import kz.greetgo.blog.stand.bean_containers.StandBeanContainer;
+
+
+public class StandLauncher {
+    public static void main(String [] args) throws Exception {
+        new StandLauncher().run();
+    }
+    private static void run() throws Exception {
+        DepinjectUtil.implementAndUseBeanContainers("kz.greetgo.blog.stand",
+                Modules.standDir()+"/build/src_bean_container");
+
+
+        StandBeanContainer standBeanContainer= Depinject.newInstance(StandBeanContainer.class);
+        standBeanContainer.standServer().start();
+    }
+}
